af11e020f779d13bb7a04968a1a762e13929ea85
yuki.shishido <yuki.shishido@optim.co.jp>
2023-07-22 16:49:09 +0900

    ログアウト機能のエラー改修(#8)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # ログアウト用のルートを設定する
+  devise_scope :user do
+    delete 'users/sign_out', to: 'devise/sessions#destroy', as: :custom_destroy_user_session
+  end
+
   get 'comments/index'
-  root 'comments#index'  # コメント一覧をルートパスとして設定する
+  root 'comments#index'
 end
